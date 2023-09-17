bf96bee50aa356171e21dcc379a9d0d6e4063415
enekarab <116077816+enekarab@users.noreply.github.com>
2023-09-17 19:24:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+var default_items = [
+``
+];
+var app = new Vue({
+  el:'#app',
+  data:{
+ 
+    currDeg:0,
+    items_str:'',
+  },
+  mounted:function(){
+    this.toggleItems();
+  },
+  methods:{
+    color:function(n){
+      var colors = [
+
+        '#FFD23F','#EE4266','#a94fca','#3BCEAC','#2765d4','#FF715B'
+      ];
+      return colors[n%colors.length];
+    },
+    spin:function(e){
+      var that = this;
+      const lengthOfSpin = 3000;//needs to match css
+      const startingDeg = Number(that.currDeg) || 500
+      that.currDeg = startingDeg + Math.round(Math.random() * (3000 - 360) + 360)
+  
+
+    },
+    textPercentage:function(n){
+      return  90 + (n * (360/this.items.length)) + (360/(this.items.length*2));
+    },
+    toggleItems:function(){
+      if(this.items_str!=default_items[0]){
+        this.items_str = default_items[0];
+      }
+      else{
+        this.items_str = default_items[1];
+      }
+    }
+  },
+  computed:{
+    items:function(){
+      return this.items_str.split("\n");
+    },
+    size:function(){
+      return 100/this.items.length;
+    },
+  }
+});
